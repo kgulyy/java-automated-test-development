e2a0dc1a27eee4b31b13fabbc7f08c168adc67c4
Konstantin Gulyy <kgulyy@my.com>
2020-05-22 17:06:53 +0300

    Use junit5 instead of junit4

--- src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
+++ src/test/java/com/acme/banking/dbo/domain/SavingAccountTest.java
@@ -1,36 +1,40 @@
 package com.acme.banking.dbo.domain;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SavingAccountTest {
+class SavingAccountTest {
     private static final double DUMMY_AMOUNT = 0;
     private static final UUID DUMMY_ID = UUID.randomUUID();
     private static final UUID DUMMY_CLIENT_ID = UUID.randomUUID();
     private static final Client DUMMY_CLIENT = new Client(DUMMY_CLIENT_ID, "client name");
 
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldNotCreateWhenIdIsNull() {
-        new SavingAccount(null, DUMMY_CLIENT, DUMMY_AMOUNT);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldNotCreateWhenClienIsNull() {
-        new SavingAccount(DUMMY_ID, null, DUMMY_AMOUNT);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldNotCreateWhenAmountIsLessThanZero() {
-        final double dummyNegativeAmount = -1;
-
-        new SavingAccount(DUMMY_ID, DUMMY_CLIENT, dummyNegativeAmount);
+    @Test
+    void shouldNotCreateWhenIdIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new SavingAccount(null, DUMMY_CLIENT, DUMMY_AMOUNT));
     }
 
     @Test
-    public void shouldSavePropertiesWhenCreated() {
+    void shouldNotCreateWhenClienIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new SavingAccount(DUMMY_ID, null, DUMMY_AMOUNT));
+    }
+
+    @Test
+    void shouldNotCreateWhenAmountIsLessThanZero() {
+        final double dummyNegativeAmount = -1;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new SavingAccount(DUMMY_ID, DUMMY_CLIENT, dummyNegativeAmount));
+    }
+
+    @Test
+    void shouldSavePropertiesWhenCreated() {
         final SavingAccount sut = new SavingAccount(DUMMY_ID, DUMMY_CLIENT, DUMMY_AMOUNT);
 
         assertThat(sut.getId()).isNotNull().isEqualTo(DUMMY_ID);
